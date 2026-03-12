58aa84494cc36e2869bb89b052a98920cb36acd5
mikepad792-gif <mikepad792@gmail.com>
2026-03-12 14:17:23 -0700

    Delete netlify/functions directory

--- netlify/functions/admin.js
+++
@@ -1,9 +0,0 @@
-
-exports.handler = async (event) => {
-  const secret = process.env.MIKE_SECRET;
-  const provided = event.headers["x-admin-secret"] || event.headers["X-Admin-Secret"];
-  if (!secret || provided !== secret) {
-    return { statusCode: 403, body: "forbidden" };
-  }
-  return { statusCode: 200, body: "admin ok" };
-};
